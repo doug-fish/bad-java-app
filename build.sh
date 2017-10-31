591f3057b78eb0017effc536efcf5a8e014f361d
Doug Fish <Fish.Douglas@mayo.edu>
2017-10-31 13:20:01 -0500

    functional, but inefficient build

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 docker build -t bad-java-build -f Dockerfile-b .
-docker run --name bad-build bad-java-build
+docker run -v /Users/m164032/bad-java-test-app/src:/src --name bad-build bad-java-build
 docker cp bad-build:/target/bad-java-test-app-1.0.0.war ./target/bad-java-test-app-1.0.0.war
 docker rm bad-build
 
